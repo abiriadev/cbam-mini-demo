a2e9497924cdf7194c4294cd1a83cc3289bda48b
Abiria <abiria.dev@gmail.com>
2024-01-15 17:30:40 +0900

    build: add `@layouts` directory

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
+			'@layouts': resolve(__dirname, './src/layouts'),
 			'@components': resolve(
 				__dirname,
 				'./src/components',
