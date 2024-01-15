24a8c9b698e860ce48db1ce3763e7b2242e91b63
Abiria <abiria.dev@gmail.com>
2024-01-15 17:55:34 +0900

    feat: add title content

--- src/components/TitledTable.tsx
+++ src/components/TitledTable.tsx
@@ -1,9 +1,44 @@
-import { Table } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import {
+	Button,
+	Flex,
+	Table,
+	Typography,
+	theme,
+} from 'antd'
 
 export interface TitledTableProps {
 	id: string
 }
 
 export const TitledTable = ({ id }: TitledTableProps) => {
-	return <Table id={id} />
+	const { token } = theme.useToken()
+
+	return (
+		<Table
+			id={id}
+			pagination={false}
+			title={() => (
+				<Flex justify="space-between">
+					<Typography.Title level={4}>
+						Processes
+					</Typography.Title>
+					<Button
+						type="primary"
+						icon={<PlusOutlined />}
+					>
+						<Typography.Text
+							strong
+							style={{
+								color: token.Button
+									?.primaryColor,
+							}}
+						>
+							Add new process
+						</Typography.Text>
+					</Button>
+				</Flex>
+			)}
+		/>
+	)
 }
