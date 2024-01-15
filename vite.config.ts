58d8a9b74ef06b13b85445cdce04e043a648627f
Abiria <abiria.dev@gmail.com>
2024-01-15 17:28:57 +0900

    build: add alias setting to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@components': resolve(
+				__dirname,
+				'./src/components',
+			),
+		},
+	},
 })
