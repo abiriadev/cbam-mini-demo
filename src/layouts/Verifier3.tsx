bfbcf0ddc0948177f39d3567d0985a403d704dcf
Abiria <abiria.dev@gmail.com>
2024-01-15 17:42:46 +0900

    refactor: move `<Verifier3 />`

--- src/layouts/Verifier3.tsx
+++ src/layouts/Verifier3.tsx
@@ -1,5 +1,5 @@
 import { Flex, Form, Typography, theme } from 'antd'
-import { I } from './I'
+import { I } from '@/I'
 
 export const Verifier3 = ({ id }: { id?: string }) => {
 	const { token } = theme.useToken()
