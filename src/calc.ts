ba9c6f0bf98aa02fbbf5df912b420dd7e754c83c
Abiria <abiria.dev@gmail.com>
2024-01-19 15:09:04 +0900

    feat: add state for 'about the installation' section

--- src/calc.ts
+++ src/calc.ts
@@ -5,12 +5,44 @@ export interface Nemesia {
 		start: Date
 		end: Date
 	}
+	a_2: {
+		name: string
+		name_en: string
+		street: string
+		economic: string
+		zip: string
+		po: string
+		city: string
+		country: string
+		unlocode: string
+		latitude: string
+		longitude: string
+		representative: string
+		email: string
+		telephone: string
+	}
 }
 
 const init: Nemesia = {
 	a_1: {
 		start: new Date('2023-01-01'),
 		end: new Date('2023-12-31'),
+	},
+	a_2: {
+		name: 'name',
+		name_en: 'name_en',
+		street: 'street',
+		economic: '??',
+		zip: '12345',
+		po: 'po box',
+		city: 'Seoul',
+		country: 'Republic of Korea',
+		unlocode: '11??',
+		latitude: '123',
+		longitude: '1123',
+		representative: '??',
+		email: 'aaa@aaa.com',
+		telephone: '232323',
 	},
 }
 
