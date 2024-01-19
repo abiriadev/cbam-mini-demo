63d5f7a69b7b6eaa2470845ed35b5d8d7b8e5382
Abiria <abiria.dev@gmail.com>
2024-01-19 15:34:20 +0900

    feat: render `Verifier2`

--- src/layouts/Verifier2.tsx
+++ src/layouts/Verifier2.tsx
@@ -1,8 +1,14 @@
 import { Flex, Form, Typography, theme } from 'antd'
 import { I } from '@/I'
+import { useSelector } from 'react-redux'
+import { selectNemesia } from '@/calc'
 
 export const Verifier2 = ({ id }: { id?: string }) => {
 	const { token } = theme.useToken()
+	const nemesia = useSelector(selectNemesia).a_3_2
+	const [form] = Form.useForm()
+
+	form.setFieldsValue(nemesia)
 
 	return (
 		<div
@@ -25,7 +31,7 @@ export const Verifier2 = ({ id }: { id?: string }) => {
 				</Typography.Title>
 			</Flex>
 			<Flex gap="large">
-				<Form>
+				<Form form={form}>
 					<I label="Name" name="name" />
 					<I label="Email address" name="email" />
 					<I
