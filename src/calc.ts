41c3416e6023751783aa73710043fef0238d6845
Abiria <abiria.dev@gmail.com>
2024-01-19 14:40:04 +0900

    feat: add init data

--- src/calc.ts
+++ src/calc.ts
@@ -1,6 +1,19 @@
+import { RootState } from './store'
+
 export interface Nemesia {
 	a_1: {
 		start: Date
 		end: Date
 	}
+}
+
+const init: Nemesia = {
+	a_1: {
+		start: new Date('2023-01-01'),
+		end: new Date('2023-12-31'),
+	},
+}
+
+export const selectNemesia = ({ cbam }: RootState) => {
+	return init
 }
