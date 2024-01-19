db8e746bb80c2ebd583974b8c0c6069169f08993
Abiria <abiria.dev@gmail.com>
2024-01-19 20:07:58 +0900

    feat: bind data

--- src/layouts/SummaryOfTheInstallation.tsx
+++ src/layouts/SummaryOfTheInstallation.tsx
@@ -1,10 +1,14 @@
+import { selectNemesia } from '@/calc'
 import { Table, Typography } from 'antd'
+import { useSelector } from 'react-redux'
 
 export const SummaryOfTheInstallation = ({
 	id,
 }: {
 	id?: string
 }) => {
+	const nemesia = useSelector(selectNemesia).a_2
+
 	return (
 		<Table
 			id={id}
@@ -16,31 +20,36 @@ export const SummaryOfTheInstallation = ({
 			)}
 			columns={[
 				{ dataIndex: 'k' },
-				{
-					render: () => 'value',
-				},
+				{ dataIndex: 'v' },
 			]}
 			dataSource={[
 				{
 					k: 'Name of the installation (English name)',
+					v: nemesia.name,
 				},
 				{
 					k: 'Street, Number',
+					v: nemesia.name_en,
 				},
 				{
 					k: 'Economic activity',
+					v: nemesia.economic,
 				},
 				{
 					k: 'Country',
+					v: nemesia.country,
 				},
 				{
 					k: 'UNLOCODE',
+					v: nemesia.unlocode,
 				},
 				{
 					k: 'Coordinates of the main emission source (latitude)',
+					v: nemesia.latitude,
 				},
 				{
 					k: 'Coordinates of the main emission source (longitude)',
+					v: nemesia.longitude,
 				},
 			]}
 		/>
