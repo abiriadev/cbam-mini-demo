c7b4933fe8c5840d8fa0aba93871ef149775c24f
Abiria <abiria.dev@gmail.com>
2024-01-19 20:32:32 +0900

    feat: bind dataindex

--- src/layouts/SummaryProducts.tsx
+++ src/layouts/SummaryProducts.tsx
@@ -12,36 +12,47 @@ export const SummaryProducts = ({ id }: { id: string }) => {
 			columns={[
 				{
 					title: 'Process',
+					dataIndex: 'name',
 				},
 				{
 					title: 'AGC',
+					dataIndex: 'agc',
 				},
 				{
 					title: 'CN Name',
+					dataIndex: 'cn',
 				},
 				{
 					title: 'Product name',
+					dataIndex: 'product_name',
 				},
 				{
 					title: 'SEE (direct)',
+					dataIndex: 'see_d',
 				},
 				{
 					title: 'SEE (indirect)',
+					dataIndex: 'see_i',
 				},
 				{
 					title: 'SEE (total)',
+					dataIndex: 'see_t',
 				},
 				{
 					title: 'Unit',
+					dataIndex: 'unit',
 				},
 				{
 					title: 'Share of emissions',
+					dataIndex: 'share',
 				},
 				{
 					title: 'Source for electricity EF',
+					dataIndex: 'source',
 				},
 				{
 					title: 'Embedded electricity',
+					dataIndex: 'ee',
 				},
 			]}
 			dataSource={[]}
