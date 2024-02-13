f24e65ad536d6cff362177e6f375291098dc3d6e
Abiria <abiria.dev@gmail.com>
2024-02-13 09:49:49 +0900

    test: add a unit test from real result

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -1,21 +1,27 @@
-import { calc } from '@/calc'
+import { calc, emInstCombustion } from '@/calc'
 import { CbamState } from '@/store/cbam'
 import { RootState } from '@/store'
-import { initialState } from '@/store/cbam'
+// import { initialState } from '@/store/cbam'
 
 const genRootState = (cbam: CbamState): RootState => ({
 	cbam,
 	environment: { state: 'generated', isIframe: false },
 })
 
-it('A', () => {
-	expect(calc(initialState)).toHaveProperty('s2.list', [])
-})
-
-// it('should produce same output as the sheet', () => {
-// 	expect(calc(initialState)).toEqual({
-// 		{
-//
-// 		}
-// 	})
+// it('A', () => {
+// 	expect(calc(initialState)).toHaveProperty('s2.list', [])
 // })
+
+describe('emInst', () => {
+	it('combustion 1', () => {
+		expect(
+			emInstCombustion({
+				ad: 100,
+				ef: 2,
+			}),
+		).toEqual({
+			fossil: 200,
+			bio: 0,
+		})
+	})
+})
