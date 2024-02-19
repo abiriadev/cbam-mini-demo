2c4a2733a3cbd0132fd356dbc52fcb4488b56ee7
Abiria <abiria.dev@gmail.com>
2024-02-19 13:01:49 +0900

    chore: 나는 능이버섯이다

--- src/store/cbam/selector.ts
+++ src/store/cbam/selector.ts
@@ -1,3 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit'
+
+interface State {
+	processes: Record<Id, Process>
+	purchased_precursors: Record<Id, PurchasedPrecursor>
+}
+
 interface Emission {
 	direct: number
 	indirect: number
@@ -45,3 +52,77 @@ interface Precursor {
 interface PurchasedPrecursor extends Identifiable {
 	see: Emission
 }
+
+const byId = <T>(_: T, id: Id) => id
+
+const selectProcessById = createSelector(
+	[({ processes }: State) => processes, byId],
+	(processes, id) => processes[id],
+)
+
+const selectPurchasedPrecursors = createSelector(
+	[
+		({ purchased_precursors }: State) =>
+			purchased_precursors,
+	],
+	purchased_precursors => purchased_precursors,
+)
+
+const selectPurchasedPrecursorById = createSelector(
+	[selectPurchasedPrecursors, byId],
+	(purchased_precursors, id) => purchased_precursors[id],
+)
+
+const selectDiremById = createSelector(
+	[selectProcessById],
+	({ direm }) => direm,
+)
+
+const selectHeatById = createSelector(
+	[selectProcessById],
+	({
+		heat: {
+			imported,
+			exported,
+			ef_imported,
+			ef_exported,
+		},
+	}) => imported * ef_imported - exported * ef_exported,
+)
+
+const selectWgById = createSelector(
+	[selectProcessById],
+	({
+		wg: {
+			imported,
+			exported,
+			ef_imported,
+			ef_exported,
+		},
+	}) => imported * ef_imported - exported * ef_exported,
+)
+
+const selectAttrById = createSelector(
+	[selectDiremById, selectHeatById, selectWgById],
+	(direm, heat, wg) => direm + heat + wg,
+)
+
+const selectPrecursorsById = createSelector(
+	[selectProcessById],
+	({ precursors }) => precursors,
+)
+
+const selectEeById = createSelector(
+	[
+		selectAttrById,
+		selectPrecursorsById,
+		selectPurchasedPrecursors,
+	],
+	(attr, precursors, purchased_precursors) => {
+		precursors.purchased_precursors.map(
+			({ ref, amount }) =>
+				purchased_precursors[ref].see.direct *
+				amount,
+		)
+	},
+)
