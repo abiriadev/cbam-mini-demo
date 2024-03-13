c5879f1f8ff847cf777dcf62a1e38183d189c176
Abiria <abiria.dev@gmail.com>
2024-03-13 11:39:38 +0900

    docs: add a new story to `TitledTable`

--- src/components/TitledTable.stories.ts
+++ src/components/TitledTable.stories.ts
@@ -25,15 +25,57 @@ export const Primary: Story = {
 				title: 'Column B',
 				dataIndex: 'b',
 			},
+			{
+				title: 'Column C',
+				dataIndex: 'c',
+			},
 		],
 		dataSource: [
 			{
 				a: 1,
 				b: 2,
+				c: 3,
 			},
 			{
-				a: 3,
-				b: 4,
+				d: 4,
+				e: 5,
+				f: 6,
+			},
+		],
+	},
+}
+
+export const Button: Story = {
+	args: {
+		titleText: 'Table title',
+		button: {
+			callback() {},
+			text: 'Button',
+		},
+		columns: [
+			{
+				title: 'Column A',
+				dataIndex: 'a',
+			},
+			{
+				title: 'Column B',
+				dataIndex: 'b',
+			},
+			{
+				title: 'Column C',
+				dataIndex: 'c',
+			},
+		],
+		dataSource: [
+			{
+				a: 1,
+				b: 2,
+				c: 3,
+			},
+			{
+				d: 4,
+				e: 5,
+				f: 6,
 			},
 		],
 	},
