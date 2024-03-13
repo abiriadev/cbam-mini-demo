d35042f2da93f0614baa3584ed9d46e2df9d4aa4
Abiria <abiria.dev@gmail.com>
2024-03-13 13:37:24 +0900

    chore: remove comment

--- src/components/DeleteButton.stories.ts
+++ src/components/DeleteButton.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { DeleteButton } from './DeleteButton'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
 	title: 'DeleteButton',
 	component: DeleteButton,
