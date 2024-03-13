104a7ed742fb6a3b39707dbe8bae321cc79dd4b9
Abiria <abiria.dev@gmail.com>
2024-03-13 13:31:32 +0900

    feat: add api client for excel api

---
+++ src/excel-api.ts
@@ -0,0 +1,22 @@
+import createClient from 'openapi-fetch'
+import type { paths, components } from './api'
+
+export type CbamInput = components['schemas']['cbam']
+
+const client = createClient<paths>({
+	baseUrl:
+		process.env.VITE_EXCEL_API_ENDPOINT +
+		'/cbam/download',
+})
+
+export const createExcel = async (
+	input: CbamInput,
+): Promise<string> => {
+	const { error, response } = await client.POST('/', {
+		body: input,
+	})
+
+	if (error) throw error
+
+	return URL.createObjectURL(await response?.blob())
+}
