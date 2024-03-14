6f4691a4a52c5a95fcf0d321e3ad09885bf4aef5
Abiria <abiria.dev@gmail.com>
2024-03-14 15:11:40 +0900

    refactor: fix `AboutTheInstallation` to use new componenets

--- src/layouts/AboutTheInstallation.tsx
+++ src/layouts/AboutTheInstallation.tsx
@@ -1,86 +1,64 @@
-import { Flex, Form, Typography, theme } from 'antd'
-import { I } from '@/I'
-import { useSelector } from 'react-redux'
-import { selectNemesia } from '@/calc'
+import { Fields } from '@/components/Fields'
+import { TitledBox } from '@/components/TitledBox'
 
 export const AboutTheInstallation = ({
 	id,
 }: {
 	id?: string
 }) => {
-	const { token } = theme.useToken()
-	const nemesia = useSelector(selectNemesia).a_2
-	const [form] = Form.useForm()
-
-	form.setFieldsValue(nemesia)
-
 	return (
-		<div
-			id={id}
-			style={{
-				backgroundColor: token.colorBgContainer,
-				borderRadius: token.borderRadius,
-				padding: token.padding,
-			}}
-		>
-			<Flex
-				justify="space-between"
-				style={{
-					paddingBottom: token.padding,
-				}}
-			>
-				<Typography.Title level={4}>
-					About the installation
-				</Typography.Title>
-			</Flex>
-			<Flex gap="large">
-				<Form form={form} layout="vertical">
-					<I
-						label="Name of the installation (optional)"
-						name="name"
-					/>
-					<I
-						label="Name of the installation (English name)"
-						name="name_en"
-						required
-					/>
-					<I
-						label="Street, Number"
-						name="street"
-					/>
-					<I
-						label="Economic activity"
-						name="economic"
-					/>
-					<I label="Post code" name="zip" />
-					<I label="P.O. Box" name="po" />
-					<I label="City" name="city" />
-					<I
-						label="Country"
-						name="country"
-						required
-					/>
-					<I
-						label="UNLOCODE"
-						name="unlocode"
-						required
-					/>
-					<I
-						label="Coordinates of the main emission source (latitude)"
-						name="latitude"
-					/>
-					<I
-						label="Coordinates of the main emission source (longitude)"
-						name="longitude"
-					/>
-					<I
-						label="Name of authorized representative"
-						name="representative"
-					/>
-					<I label="Email" name="email" />
-					<I label="Telephone" name="telephone" />
-				</Form>
-			</Flex>
-		</div>
+		<TitledBox id={id} title="About the installation">
+			<Fields
+				fields={[
+					{
+						label: 'Name of the installation (optional)',
+						name: 'name',
+					},
+					{
+						label: 'Name of the installation (English name)',
+						name: 'name_en',
+						required: true,
+					},
+					{
+						label: 'Street, Number',
+						name: 'street',
+					},
+					{
+						label: 'Economic activity',
+						name: 'economic',
+					},
+					{ label: 'Post code', name: 'zip' },
+					{ label: 'P.O. Box', name: 'po' },
+					{ label: 'City', name: 'city' },
+					{
+						label: 'Country',
+						name: 'country',
+						required: true,
+					},
+					{
+						label: 'UNLOCODE',
+						name: 'unlocode',
+						required: true,
+					},
+					{
+						label: 'Coordinates of the main emission source (latitude)',
+						name: 'latitude',
+					},
+					{
+						label: 'Coordinates of the main emission source (longitude)',
+						name: 'longitude',
+					},
+					{
+						label: 'Name of authorized representative',
+						name: 'representative',
+					},
+					{ label: 'Email', name: 'email' },
+					{
+						label: 'Telephone',
+						name: 'telephone',
+					},
+				]}
+			></Fields>
+		</TitledBox>
 	)
 }
