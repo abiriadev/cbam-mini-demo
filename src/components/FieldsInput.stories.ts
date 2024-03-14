9f4598b0a93f85978c89bb2d01781174bdab6cf3
Abiria <abiria.dev@gmail.com>
2024-03-14 11:49:50 +0900

    docs: add a story for `FieldsInput`

---
+++ src/components/FieldsInput.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { FieldsInput } from './FieldsInput'
+
+const meta = {
+	title: 'FieldsInput',
+	component: FieldsInput,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof FieldsInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+	args: {
+		label: 'Label',
+	},
+}
