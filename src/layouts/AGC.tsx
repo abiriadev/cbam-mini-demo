36e842f43522d9badc7a5b96b194e868ecabdbf0
Abiria <abiria.dev@gmail.com>
2024-03-15 16:19:18 +0900

    feat: apply components

--- src/layouts/AGC.tsx
+++ src/layouts/AGC.tsx
@@ -1,53 +1,23 @@
-import {
-	Button,
-	Dropdown,
-	Input,
-	Popconfirm,
-	Typography,
-	theme,
-} from 'antd'
+import { Button, Dropdown, Input, Typography } from 'antd'
 import { useSelector } from 'react-redux'
-import {
-	DeleteOutlined,
-	DownOutlined,
-} from '@ant-design/icons'
 import { AgcKindSet } from '@/store/cbam'
 import { selectNemesia } from '@/calc'
 import { TitledTable } from '@/components/TitledTable'
 import { DeleteButton } from '@/components/DeleteButton'
+import { DropdownButton } from '@/components/DropdownButton'
 
 export const AGC = ({ id }: { id?: string }) => {
 	const nemesia = useSelector(selectNemesia).a_4_1
-
-	const { token } = theme.useToken()
 
 	return (
 		<TitledTable
 			id={id}
 			titleText="List of aggregated goods categories"
 			button={
-				<Dropdown
-					trigger={['click']}
-					menu={{
-						items: AgcKindSet.map(k => ({
-							key: k,
-							label: k,
-						})),
-					}}
-				>
-					<Button
-						type="primary"
-						icon={<DownOutlined />}
-					>
-						<Typography.Text
-							strong
-							className="text-white"
-						>
-							Add new aggregated goods
-							category
-						</Typography.Text>
-					</Button>
-				</Dropdown>
+				<DropdownButton
+					text="Add new aggregated goods category"
+					items={AgcKindSet}
+				/>
 			}
 			rowKey={({ id }) => id}
 			columns={[
