782623af16578f0388e67100523aa2035b8eacab
Abiria <abiria.dev@gmail.com>
2024-03-15 11:37:24 +0900

    refactor: use components

--- src/layouts/Verifier3.tsx
+++ src/layouts/Verifier3.tsx
@@ -1,51 +1,28 @@
-import { Flex, Form, Typography, theme } from 'antd'
-import { I } from '@/I'
-import { useSelector } from 'react-redux'
-import { selectNemesia } from '@/calc'
+import { TitledBox } from '@/components/TitledBox'
+import { Fields } from '@/components/Fields'
 
 export const Verifier3 = ({ id }: { id?: string }) => {
-	const { token } = theme.useToken()
-	const nemesia = useSelector(selectNemesia).a_3_3
-	const [form] = Form.useForm()
-
-	form.setFieldsValue(nemesia)
-
 	return (
-		<div
+		<TitledBox
 			id={id}
-			style={{
-				backgroundColor: token.colorBgContainer,
-				borderRadius: token.borderRadius,
-				padding: token.padding,
-			}}
+			title="Information about the verifier's accreditation"
 		>
-			<Flex
-				justify="space-between"
-				style={{
-					paddingBottom: token.padding,
-				}}
-			>
-				<Typography.Title level={4}>
-					Information about the verifier's
-					accreditation
-				</Typography.Title>
-			</Flex>
-			<Flex gap="large">
-				<Form form={form} layout="vertical">
-					<I
-						label="Accreditation Member State"
-						name="state"
-					/>
-					<I
-						label="Name of the national accreditation body"
-						name="accreditation"
-					/>
-					<I
-						label="Registration number issued by the Accreditation body"
-						name="reg"
-					/>
-				</Form>
-			</Flex>
-		</div>
+			<Fields
+				fields={[
+					{
+						label: 'Accreditation Member State',
+						name: 'state',
+					},
+					{
+						label: 'Name of the national accreditation body',
+						name: 'accreditation',
+					},
+					{
+						label: 'Registration number issued by the Accreditation body',
+						name: 'reg',
+					},
+				]}
+			/>
+		</TitledBox>
 	)
 }
