df0c951424f71da349963889fac29e1394760c0c
Abiria <abiria.dev@gmail.com>
2024-03-21 17:58:31 +0900

    docs: group repeated story props

--- src/components/TitledTable.stories.tsx
+++ src/components/TitledTable.stories.tsx
@@ -9,72 +9,44 @@ const meta = {
 		layout: 'centered',
 	},
 	tags: ['autodocs'],
+	args: {
+		titleText: 'Table title',
+		columns: [
+			{
+				title: 'Column A',
+				dataIndex: 'a',
+			},
+			{
+				title: 'Column B',
+				dataIndex: 'b',
+			},
+			{
+				title: 'Column C',
+				dataIndex: 'c',
+			},
+		],
+		dataSource: [
+			{
+				a: 1,
+				b: 2,
+				c: 3,
+			},
+			{
+				a: 4,
+				b: 5,
+				c: 6,
+			},
+		],
+	},
 } satisfies Meta<typeof TitledTable>
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
-	args: {
-		titleText: 'Table title',
-		columns: [
-			{
-				title: 'Column A',
-				dataIndex: 'a',
-			},
-			{
-				title: 'Column B',
-				dataIndex: 'b',
-			},
-			{
-				title: 'Column C',
-				dataIndex: 'c',
-			},
-		],
-		dataSource: [
-			{
-				a: 1,
-				b: 2,
-				c: 3,
-			},
-			{
-				d: 4,
-				e: 5,
-				f: 6,
-			},
-		],
-	},
-}
+export const Primary: Story = {}
 
 export const Button: Story = {
 	args: {
-		titleText: 'Table title',
 		button: <PlusButton>Button</PlusButton>,
-		columns: [
-			{
-				title: 'Column A',
-				dataIndex: 'a',
-			},
-			{
-				title: 'Column B',
-				dataIndex: 'b',
-			},
-			{
-				title: 'Column C',
-				dataIndex: 'c',
-			},
-		],
-		dataSource: [
-			{
-				a: 1,
-				b: 2,
-				c: 3,
-			},
-			{
-				d: 4,
-				e: 5,
-				f: 6,
-			},
-		],
 	},
 }
