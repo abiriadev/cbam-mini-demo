a4b424a673bd004c135d3e68900f34499aab4cc0
Abiria <abiria.dev@gmail.com>
2024-03-21 16:41:57 +0900

    build: apply tailwindcss

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import type { Preview } from '@storybook/react'
 import { ConfigProvider } from 'antd'
 import { theme } from '../src/theme'
+import '../src/index.css'
 
 const preview: Preview = {
 	parameters: {
