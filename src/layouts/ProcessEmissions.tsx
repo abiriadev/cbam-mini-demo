3f5624535fd5a686737a9e441c9c5c4755791303
Abiria <abiria.dev@gmail.com>
2024-03-22 16:35:03 +0900

    refactor: use `<TitledTable />`

--- src/layouts/ProcessEmissions.tsx
+++ src/layouts/ProcessEmissions.tsx
@@ -2,6 +2,7 @@ import { Table, Typography } from 'antd'
 import { useSelector } from 'react-redux'
 import { fixEmission, nanGuard, sum } from '@/utils'
 import { selectNemesia } from '@/calc'
+import { TitledTable } from '@/components/TitledTable'
 
 export const ProcessEmissions = ({
 	id,
@@ -11,56 +12,50 @@ export const ProcessEmissions = ({
 	const nemesia = useSelector(selectNemesia).s1_2_2_1
 
 	return (
-		<div id={id}>
-			<Table
-				title={() => (
-					<Typography.Title level={4}>
-						Process Emissions
-					</Typography.Title>
-				)}
-				rowKey={({ id }) => id}
-				pagination={false}
-				columns={[
-					{
-						title: 'Name',
-						dataIndex: 'name',
-					},
-					{
-						title: 'SE (direct)',
-						dataIndex: ['se', 'direct'],
-					},
-					{
-						title: 'SE (indirect)',
-						dataIndex: ['se', 'indirect'],
-					},
-					{
-						title: 'SE (total)',
-					},
-					{
-						title: 'SEE (direct)',
-						dataIndex: ['see', 'direct'],
-					},
-					{
-						title: 'SEE (indirect)',
-						dataIndex: ['see', 'indirect'],
-					},
-					{
-						title: 'SEE (total)',
-					},
-					{
-						title: 'EmbEm (direct)',
-						dataIndex: ['ee', 'direct'],
-					},
-					{
-						title: 'EmbEm (indirect)',
-						dataIndex: ['ee', 'indirect'],
-					},
-					{
-						title: 'EmbEm (total)',
-					},
-				]}
-				dataSource={nemesia.list}
-			/>
-		</div>
+		<TitledTable
+			id={id}
+			titleText="Process Emissions"
+			rowKey={({ id }) => id}
+			columns={[
+				{
+					title: 'Name',
+					dataIndex: 'name',
+				},
+				{
+					title: 'SE (direct)',
+					dataIndex: ['se', 'direct'],
+				},
+				{
+					title: 'SE (indirect)',
+					dataIndex: ['se', 'indirect'],
+				},
+				{
+					title: 'SE (total)',
+				},
+				{
+					title: 'SEE (direct)',
+					dataIndex: ['see', 'direct'],
+				},
+				{
+					title: 'SEE (indirect)',
+					dataIndex: ['see', 'indirect'],
+				},
+				{
+					title: 'SEE (total)',
+				},
+				{
+					title: 'EmbEm (direct)',
+					dataIndex: ['ee', 'direct'],
+				},
+				{
+					title: 'EmbEm (indirect)',
+					dataIndex: ['ee', 'indirect'],
+				},
+				{
+					title: 'EmbEm (total)',
+				},
+			]}
+			dataSource={nemesia.list}
+		/>
 	)
 }
