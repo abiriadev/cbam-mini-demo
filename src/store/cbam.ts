7a74316121c08a1f76549535948628167a224437
Abiria <abiria.dev@gmail.com>
2024-03-26 10:19:50 +0900

    feat: define input and output type

--- src/store/cbam.ts
+++ src/store/cbam.ts
@@ -1,8 +1,21 @@
+import { CbamInput } from '@/excel-api'
 import { createSlice } from '@reduxjs/toolkit'
+
+export interface CbamState {
+	i: CbamInput
+	o: CbamOutput
+}
+
+export interface CbamOutput {}
+
+const zeroback: CbamState = {
+	i: todo!(),
+	o: todo!(),
+}
 
 export const cbamSlice = createSlice({
 	name: 'cbam',
-	initialState: aaa,
+	initialState: zeroBack,
 	reducers: {},
 })
 
