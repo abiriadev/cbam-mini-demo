e8907cc44603310228a77b51341110f204f4ce3c
Abiria <abiria.dev@gmail.com>
2024-03-26 10:29:58 +0900

    chore: remove unused `id`

--- src/layouts/SheetsList.tsx
+++ src/layouts/SheetsList.tsx
@@ -31,82 +31,69 @@ export const SheetsList = ({ roww }: { roww: number }) => {
 		<Row>
 			<Col span={roww}>
 				<Flex vertical gap="large">
-					<Sheet id="instdata" title="A InstData">
-						<ReportingPeriod id="reporting-period" />
-						<AboutTheInstallation id="about-the-installation" />
+					<Sheet title="A InstData">
+						<ReportingPeriod />
+						<AboutTheInstallation />
 						<Sheet
-							id="verifier-of-the-report"
 							title="Verifier of the report"
 							sub
 						>
-							<Verifier1 id="verifier1" />
-							<Verifier2 id="verifier2" />
-							<Verifier3 id="verifier3" />
+							<Verifier1 />
+							<Verifier2 />
+							<Verifier3 />
 						</Sheet>
 						<Sheet
-							id="aggregated-goods-category"
 							title="Aggregated goods categories and relevant production processes"
 							sub
 						>
-							<AGC id="list-of-aggregated-goods-categories" />
-							<RelevantProductionProcesses id="A-4-b" />
+							<AGC />
+							<RelevantProductionProcesses />
 						</Sheet>
-						<Precursors id="A-5" />
+						<Precursors />
 					</Sheet>
-					<Sheet id="B" title="B EmInst">
-						<SourceStreams id="B-1" />
-						<PFC id="B-2" />
-						<EmissionsSources id="B-3" />
+					<Sheet title="B EmInst">
+						<SourceStreams />
+						<PFC />
+						<EmissionsSources />
 					</Sheet>
-					<Sheet
-						id="C"
-						title="C Emissions & Energy"
-					>
-						<FuelBalance id="C-1" />
-						<GhgBalanceByType id="C-2" />
+					<Sheet title="C Emissions & Energy">
+						<FuelBalance />
+						<GhgBalanceByType />
 					</Sheet>
-					<Sheet id="D" title="Processes">
+					<Sheet title="Processes">
 						<Processes />
 					</Sheet>
-					<Sheet id="E" title="PurchPrec">
-						<Precursors id="A-5" />
+					<Sheet title="PurchPrec">
+						<Precursors />
 					</Sheet>
-					<Sheet id="F" title="Tools">
-						<FuelIntoChp id="F-1-a" />
+					<Sheet title="Tools">
+						<FuelIntoChp />
 					</Sheet>
-					<Sheet
-						id="S1"
-						title="Summary Processes"
-					>
+					<Sheet title="Summary Processes">
 						<Sheet
-							id="S1-1"
 							title="Summary of the installation, processes and production routes"
 							sub
 						>
-							<SummaryOfTheInstallation id="S1-1-1" />
+							<SummaryOfTheInstallation />
 						</Sheet>
 						<Sheet
-							id="S1-2"
 							title="Greenhouse gas emissions balance and specific embedded emissions (SEE)"
 							sub
 						>
-							<GhgEmissions id="S1-2-1-a" />
-							<ProcessEmissions id="S1-2-2-a" />
-							<PrecursorEmissions id="S1-2-2-b" />
+							<GhgEmissions />
+							<ProcessEmissions />
+							<PrecursorEmissions />
 						</Sheet>
-						<Emissions id="S1-3" />
+						<Emissions />
 					</Sheet>
-					<Sheet id="S2" title="Summary Products">
-						<SummaryProducts id="summary-products" />
+					<Sheet title="Summary Products">
+						<SummaryProducts />
 					</Sheet>
-					<Sheet
-						id="S3"
-						title="Summary Communication"
-					>
-						<SummaryCommunication id="summary-communication" />
+					<Sheet title="Summary Communication">
+						<SummaryCommunication />
 					</Sheet>
-					<Sheet id="a" title="a">
-						<ProcessGroup id="aa" />
+					<Sheet title="a">
+						<ProcessGroup />
 					</Sheet>
 				</Flex>
 			</Col>
